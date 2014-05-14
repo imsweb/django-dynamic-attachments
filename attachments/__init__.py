b4450695599e5e06f9b54c22a15434ec09e88e8e
Dan Watson <watsond@imsweb.com>
2014-05-14 17:45:41 -0400

    Hopefully fix settings/import error on install

--- attachments/__init__.py
+++ attachments/__init__.py
@@ -1,5 +1,3 @@
-__version_info__ = (0, 1, 0)
-__version__ = '.'.join(str(i) for i in __version_info__)
-
 from .utils import session, get_storage
 from .models import Attachment
+from .version import *
