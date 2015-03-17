9543297f2b7c37e31a858d2e3a33dbd00d4a108c
Dan Watson <watsond@imsweb.com>
2015-03-17 13:20:52 -0400

    Removed image in favor of icon, updated migrations

---
+++ attachments/migrations/0003_auto_20150317_1319.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('attachments', '0002_property'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='property',
+            options={'verbose_name_plural': 'properties'},
+        ),
+    ]
