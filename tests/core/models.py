bb482c82eb4dc26fcd45aa36646a401074cf82a3
Dan Watson <watsond@imsweb.com>
2015-03-26 13:46:42 -0400

    New unit test for JSONField

--- tests/core/models.py
+++ tests/core/models.py
@@ -1,1 +1,5 @@
 from django.db import models
+from attachments.utils import JSONField
+
+class Document (models.Model):
+    data = JSONField()
--- tests/core/tests.py
+++ tests/core/tests.py
@@ -2,8 +2,18 @@
 
 from django.test import TestCase
 from attachments.utils import url_filename
+from .models import Document
 
 class AttachmentTests (TestCase):
 
     def test_url_filename(self):
         self.assertEqual(url_filename(u'Résumé.pdf'), 'R%C3%A9sum%C3%A9.pdf')
+
+    def test_json_field(self):
+        d = {
+            'key': 'value',
+            'number': 42,
+        }
+        Document.objects.create(data=d)
+        doc = Document.objects.get()
+        self.assertEqual(doc.data, d)
