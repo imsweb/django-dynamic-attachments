9af1bb0162028beb199a6003dec8edebd6b5f0b1
Dan Watson <watsond@imsweb.com>
2015-04-21 15:02:45 -0400

    Allow has_attachment_properties to take a ContentType, check when rendering

--- attachments/templatetags/attachments.py
+++ attachments/templatetags/attachments.py
@@ -6,9 +6,10 @@ from ..models import Property
 register = template.Library()
 
 @register.filter
-def has_attachment_properties(obj):
-    ct = ContentType.objects.get_for_model(obj)
-    return Property.objects.filter(content_type=ct).exists()
+def has_attachment_properties(content_type):
+    if not isinstance(content_type, ContentType):
+        content_type = ContentType.objects.get_for_model(content_type)
+    return Property.objects.filter(content_type=content_type).exists()
 
 @register.filter
 def attachment_properties_form(obj, content_type=None):
