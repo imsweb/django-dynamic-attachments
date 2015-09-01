2fdfd0a3dde1f58ab7b7d02f3c70a1c2cccdb031
Chris Wassenius <wassenic@imsweb.com>
2015-09-01 11:56:03 -0400

    merge fix

--- attachments/static/attachments/js/attachments.js
+++ attachments/static/attachments/js/attachments.js
@@ -9,7 +9,7 @@
             progress: null,
             success: null
         }, options);
-
+        
         var refresh = function() {
             return $.ajax({
                 url: settings.url,
@@ -18,13 +18,13 @@
                 }
             });
         };
-
+        
         var resetInput = function(input) {
             var inp = $(input);
             inp.wrap('<form>').closest('form').get(0).reset();
             inp.unwrap();
         };
-
+        
         var handleResponse = function(data, signal) {
             if(data.ok) {
                 if(settings.success) {
@@ -39,7 +39,7 @@
                 signal.reject(data);
             }
         };
-
+        
         var iframeUpload = function(input) {
             var signal = $.Deferred();
             $.ajax(settings.url, {
@@ -56,12 +56,12 @@
             });
             return signal;
         };
-
+        
         var upload = function(file) {
             var signal = $.Deferred();
             var formData = new FormData();
             formData.append('attachment', file);
-
+            
             var xhr = new XMLHttpRequest();
             if(settings.progress) {
                 xhr.upload.addEventListener('progress', function(evt) {
@@ -71,22 +71,22 @@
                     }
                 }, false);
             }
-
+            
             xhr.onload = function(evt) {
                 // Refresh the attachments listing
                 refresh();
-
+                
                 // Fire the success/error handlers with the returned JSON
                 var data = JSON.parse(xhr.responseText);
                 handleResponse(data, signal);
             };
-
+            
             xhr.open('POST', settings.url, true);
             xhr.send(formData);
-
+            
             return signal;
         };
-
+        
         var uploadFiles = function(files, input) {
             var chain = null;
             // Call all the upload methods sequentially.
@@ -98,7 +98,7 @@
                 resetInput(input);
             }
         };
-
+        
         if(settings.dropTarget) {
             $(settings.dropTarget).on({
                 dragover: function(e) {
@@ -117,10 +117,10 @@
                 }
             });
         }
-
+        
         // Load the initial attachments container.
         refresh();
-
+        
         return this.change(function(e) {
             if(typeof FormData === 'undefined') {
                 iframeUpload(this);
@@ -130,7 +130,7 @@
             }
         });
     };
-
+    
     $('body').on('click', 'a.delete-upload', function() {
         var row = $(this).closest('.upload-item');
         $.ajax({
@@ -142,7 +142,7 @@
         });
         return false;
     });
-
+    
     $('body').on('submit', '.update-attachment', function(e) {
         var postData = $(this).serializeArray();
         var formURL = $(this).attr('action');
