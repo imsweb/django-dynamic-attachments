5a0db81f5ccb06afa5ecda12c27ddd85452caf21
Feng, Andy (IMS) <fenga@imsweb.com>
2017-02-09 16:07:27 -0500

    Added Property field validation on 'model'

--- attachments/admin.py
+++ attachments/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
+from django import forms
 from .models import Attachment, Property, Session, Upload
+from .utils import import_class
 
 class AttachmentAdmin (admin.ModelAdmin):
     list_display = ('file_path', 'file_name', 'file_size', 'content_type', 'context', 'date_created')
     readonly_fields = ('data',)
+    
+class PropertyForm (forms.ModelForm):
+
+    def clean_model(self):
+        model = self.cleaned_data.get('model')
+        if model:
+            try:
+                import_class(model)
+            except:
+                raise forms.ValidationError("Improper path to lookup model.")
+        return self.cleaned_data['model']
 
 class PropertyAdmin (admin.ModelAdmin):
+    form = PropertyForm
     list_display = ('label', 'slug', 'data_type', 'choices', 'model', 'required')
     prepopulated_fields = {'slug': ('label',)}
     filter_horizontal = ('content_type',)
