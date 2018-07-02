d212cd7a325f235c11a9b161c52222737db4459c
Brad Rutten <RuttenB@imsweb.com>
2018-07-02 13:38:52 -0400

    pep8 spacing update

--- attachments/templatetags/attachments.py
+++ attachments/templatetags/attachments.py
@@ -23,9 +23,11 @@ def has_edditable_attachment_properties(content_type, excluded_slugs=""):
     excluded_slugs = excluded_slugs.split(",") if excluded_slugs else getattr(settings, "ATTACHMENTS_EDIT_EXCLUDE_PROPERTY_SLUGS", [])
     return Property.objects.exclude(slug__in=excluded_slugs).filter(content_type=content_type).exists()
 
+
 @register.filter
 def attachment_properties_form(obj):
     return PropertyForm(instance=obj)
+
 
 @register.simple_tag
 def attachment_properties_form(obj, excluded_slugs=""):
