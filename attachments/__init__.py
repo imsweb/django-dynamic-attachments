9c08284fb359dc3d8d8219488d520c0495d89741
salsburj <SalsburyJ@imsweb.com>
2020-03-24 10:47:57 -0400

    bioshare1810 - fix accidental placement of file and make AttachmentsMixin available

--- attachments/__init__.py
+++ attachments/__init__.py
@@ -1,6 +1,3 @@
-from attachments.mixins import AttachmentsMixin
-
-
 __version_info__ = (3, 1, 5)
 __version__ = '.'.join(str(i) for i in __version_info__)
 
@@ -9,3 +6,9 @@ def session(*args, **kwargs):
     # Expose utils.session without importing utils from __init__ at module level.
     from .utils import session as attachment_session
     return attachment_session(*args, **kwargs)
+
+def get_AttachmentsMixin():
+    from attachments.mixins import AttachmentsMixin
+    return AttachmentsMixin
+
+AttachmentsMixin = get_AttachmentsMixin()
