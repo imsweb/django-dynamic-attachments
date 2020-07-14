49bc0697c7979def338d8201e7c4e67146415685
Hoskins <hoskinsj@omni.imsweb.com>
2020-07-14 10:16:39 -0400

    2299 Allow global overriding of can_access permissions

--- attachments/__init__.py
+++ attachments/__init__.py
@@ -2,6 +2,9 @@ __version_info__ = (3, 2, 3)
 __version__ = '.'.join(str(i) for i in __version_info__)
 
 
+default_app_config = 'attachments.apps.AttachmentsConfig'
+
+
 def session(*args, **kwargs):
     # Expose utils.session without importing utils from __init__ at module level.
     from .utils import session as attachment_session
