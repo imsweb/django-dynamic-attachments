cc25f89547963cd216669915d6a232dfc1ad2585
brucejwb <64919223+brucejwb@users.noreply.github.com>
2023-03-08 10:47:46 -0500

    Update urls.py

--- attachments/urls.py
+++ attachments/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 
 urlpatterns = [
-    url(r'^download/(?P<attach_id>[^/]+)/(?P<filename>.*)$', views.download, name='attachment-download'),
-    url(r'^(?P<session_id>[^/]+)/$', views.attach, name='attach'),
-    url(r'^delete/upload/(?P<session_id>[^/]+)/(?P<upload_id>[^/]+)/$', views.delete_upload, name='delete-upload'),
-    url(r'^update/(?P<attach_id>[^/]+)/$', views.update_attachment, name='update-attachment'),
-    url(r'^properties/edit/(?P<attach_id>[^/]+)/$', views.edit_attachment_properties, name='edit-attachment-properties'),
-    url(r'^properties/view/(?P<attach_id>[^/]+)/$', views.view_attachment_properties, name='view-attachment-properties')
+    re_path(r'^download/(?P<attach_id>[^/]+)/(?P<filename>.*)$', views.download, name='attachment-download'),
+    re_path(r'^(?P<session_id>[^/]+)/$', views.attach, name='attach'),
+    re_path(r'^delete/upload/(?P<session_id>[^/]+)/(?P<upload_id>[^/]+)/$', views.delete_upload, name='delete-upload'),
+    re_path(r'^update/(?P<attach_id>[^/]+)/$', views.update_attachment, name='update-attachment'),
+    re_path(r'^properties/edit/(?P<attach_id>[^/]+)/$', views.edit_attachment_properties, name='edit-attachment-properties'),
+    re_path(r'^properties/view/(?P<attach_id>[^/]+)/$', views.view_attachment_properties, name='view-attachment-properties')
 ]
