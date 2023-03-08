58b586832c6579f8dda86253816d5474efe84ef4
brucejwb <64919223+brucejwb@users.noreply.github.com>
2023-03-08 10:46:59 -0500

    Update signals.py

--- attachments/signals.py
+++ attachments/signals.py
@@ -2,15 +2,15 @@ from django.dispatch import Signal
 
 
 # Sent when a file is uploaded.
-file_uploaded = Signal(providing_args=('request', 'session'))
+file_uploaded = Signal()
 
 # Sent when an attachment is downloaded.
-file_download = Signal(providing_args=('request',))
+file_download = Signal()
 
 # Sent when attachments are attached to an object (i.e. saved)
-attachments_attached = Signal(providing_args=('obj', 'attachments'))
+attachments_attached = Signal()
 
 # Sent when a virus was detected during an upload
 # NOTE: 'quarantine_path' may be None if the file was deleted instead of quarantined
 # NOTE: The 'sender' will be the Upload object representing the offending file. The actual file will have been moved or removed.
-virus_detected = Signal(providing_args=('user', 'filename', 'exception', 'time_of_upload', 'quarantine_path'))
+virus_detected = Signal()
