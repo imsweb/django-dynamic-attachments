e6897869baf6f4165c345a86ea5f6107c72e2e3d
Wrobleski <wroblesj@omni.imsweb.com>
2024-02-06 14:11:48 -0500

    3719 adding some more tests

--- testapp/tests.py
+++ testapp/tests.py
@@ -41,3 +41,66 @@ class AttachmentTests (TestCase):
         upload = sess.uploads.get()
         self.assertEqual(upload.file_name, att.name)
         self.assertEqual(upload.file_size, len(att_data))
+
+    def test_delete(self):
+        att_data = b'some data'
+        request = RequestFactory().get('/test/page/')
+        sess = session(request)
+        att = io.BytesIO(att_data)
+        att.name = 'testfile'
+        response = self.client.post('/attachments/%s/' % sess.uuid, {'attachment': att})
+        self.assertEqual(response.json(), {
+            'ok': True,
+            'file_name': att.name,
+            'file_size': len(att_data),
+        })
+        upload = sess.uploads.get()
+        response = self.client.post(f'/attachments/delete/upload/{sess.uuid}/{upload.upload_id}', {'attachment': att})
+
+        self.assertEqual(response.json(), {'ok': True})
+
+    def test_download(self):
+        att_data = b'some data'
+        request = RequestFactory().get('/test/page/')
+        sess = session(request)
+        att = io.BytesIO(att_data)
+        att.name = 'testfile'
+        response = self.client.post('/attachments/%s/' % sess.uuid, {'attachment': att})
+        self.assertEqual(response.json(), {
+            'ok': True,
+            'file_name': att.name,
+            'file_size': len(att_data),
+        })
+        upload = sess.uploads.get()
+        # run download, confirm response
+        return
+    
+    def test_update_attachment(self):
+        att_data = b'some data'
+        request = RequestFactory().get('/test/page/')
+        sess = session(request)
+        att = io.BytesIO(att_data)
+        att.name = 'testfile'
+        response = self.client.post('/attachments/%s/' % sess.uuid, {'attachment': att})
+        #upload att, run update and confirm response?
+        return
+    
+    def test_view_properties(self):
+        att_data = b'some data'
+        request = RequestFactory().get('/test/page/')
+        sess = session(request)
+        att = io.BytesIO(att_data)
+        att.name = 'testfile'
+        response = self.client.post('/attachments/%s/' % sess.uuid, {'attachment': att})
+        #upload att, run view properties, confirm response
+        return
+
+    def test_edit_properties(self):
+        att_data = b'some data'
+        request = RequestFactory().get('/test/page/')
+        sess = session(request)
+        att = io.BytesIO(att_data)
+        att.name = 'testfile'
+        response = self.client.post('/attachments/%s/' % sess.uuid, {'attachment': att})
+        #upload att, run edit properties, confirm response
+        return
